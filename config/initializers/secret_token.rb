aa43b0ec1b9c11089599e4c31f73094f93af2e0b
Hatem Mahmoud <hatem@expressionlab.com>
2012-06-24 22:04:04 +0200

    rails new rails326

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails326::Application.config.secret_token = 'bfe32e1b8ad7cbf1657e6039c36df447abe0482137b9540f0a4f1985d12f385cb3d6c431df62a9c1e22fb722b04a5af0f715b8c4219f77323f673b8f632704d6'
